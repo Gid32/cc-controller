371981da2f76a0012f19a41cd70a63e51eaf14dc
GiD32 <ivn_prmsk@ukr.net>
2024-02-17 01:11:06 +0200

    cleanup

---
+++ draft.lua
@@ -0,0 +1,31 @@
+
+---------------------------------
+
+-- turtle.select([slot])
+-- turtle.getSelectedSlot()
+
+-- turtle.getItemCount([slot])
+-- turtle.getItemSpace([slot])
+
+-- turtle.placeDown([text])
+-- turtle.placeUp([text])
+
+
+-- turtle.suck([count])
+
+-- turtle.turnRight()
+-- turtle.turnLeft()
+
+
+-- turtle.forward()
+-- turtle.back()
+-- turtle.up()
+-- turtle.down()
+
+-- turtle.detect()
+-- turtle.detectUp()
+-- turtle.detectDown()
+
+-- turtle.dig([side])
+-- turtle.digUp([side])
+-- turtle.digDown([side])
--- plane.lua
+++ plane.lua
@@ -106,34 +106,3 @@ buildHPlane(xSize, ySize)
 returnToStart(xSize, ySize)
 
 return
-
----------------------------------
-
--- turtle.select([slot])
--- turtle.getSelectedSlot()
-
--- turtle.getItemCount([slot])
--- turtle.getItemSpace([slot])
-
--- turtle.placeDown([text])
--- turtle.placeUp([text])
-
-
--- turtle.suck([count])
-
--- turtle.turnRight()
--- turtle.turnLeft()
-
-
--- turtle.forward()
--- turtle.back()
--- turtle.up()
--- turtle.down()
-
--- turtle.detect()
--- turtle.detectUp()
--- turtle.detectDown()
-
--- turtle.dig([side])
--- turtle.digUp([side])
--- turtle.digDown([side])
